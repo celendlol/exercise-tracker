b46764ab2446aff6d36236f2bd8261fae116e4d7
Ethan McMasters <emcmast@siue.edu>
2020-06-08 19:39:08 -0500

    add endpoints for exercises and setup page for creating exercises

---
+++ exercise-tracker/src/components/create-user.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CreateUser extends Component{
+    render(){
+        return (
+            <div>CREATE USER LIST Component</div>
+        )
+    }
+}
+
+export default CreateUser;
